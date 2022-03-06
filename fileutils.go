111e582c8ce0c9471bc4f68541ef48d229dbbe59
Avery Carty <avery.carty7@gmail.com>
2022-03-05 22:36:52 -0500

    added: Func WriteFileWithHashedName

--- fileutils.go
+++ fileutils.go
@@ -1,6 +1,11 @@
 package fileutils
 
-import "io/ioutil"
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/S-ign/stringutils"
+)
 
 // FiletoByte converts a file to a byte string
 func FiletoByte(filename string) ([]byte, error) {
@@ -9,4 +14,19 @@ func FiletoByte(filename string) ([]byte, error) {
 		return nil, err
 	}
 	return f, nil
+}
+
+// WriteFileWithHashedName writes file name with a hash of byte array
+func WriteFileWithHashedName(b []byte, dir string, extension string) (string, error) {
+
+	hash, err := stringutils.GenerateHash(b)
+	if err != nil {
+		return "", err
+	}
+
+	// create tmp file info
+	tmpFilename := fmt.Sprintf("%s.%s", hash, extension)
+	fullpath := fmt.Sprintf("%s/%s", dir, tmpFilename)
+
+	return fullpath, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/S-ign/fileutils
 
 go 1.17
+
+require github.com/S-ign/stringutils v0.0.0-20220306025651-44f5af680d16 // indirect
